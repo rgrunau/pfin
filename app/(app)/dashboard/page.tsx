8365570937379a0110b56130e8627034e8c05ad8
Robert Grunau <robertgrunau@hey.com>
2024-09-03 21:12:33 -0700

    scaffold accounts

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,7 +1,8 @@
-
-
-export default function DashboardPage() {
-  
+import { createClient } from "@/supabase/client"
+export default async function DashboardPage() {
+  const supabase = createClient()
+  const { data: accounts, error } = await supabase.from('accounts').select('*')
+  console.log(accounts)
   return (
     <div className="w-full min-h-full p-4 bg-slate-50 rounded-lg mt-2">
         dashboard
